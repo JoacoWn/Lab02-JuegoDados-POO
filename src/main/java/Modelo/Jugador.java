5fcbd5e8575777198e5e41a71725ab439a193a6b
Jonathan <j.chavez12@ufromail.cl>
2025-06-19 09:08:41 -0400

    Creacion de metodos de juego de dado individual

--- src/main/java/Modelo/Jugador.java
+++ src/main/java/Modelo/Jugador.java
@@ -1,4 +1,30 @@
 package Modelo;
 
 public class Jugador {
+    private final String nombre;
+    private int puntaje;
+
+    // Constructor
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.puntaje = 0;
+    }
+
+    // Métodos
+    public void incrementarPuntaje() {
+        this.puntaje++;
+    }
+
+    public void resetPuntaje() {
+        this.puntaje = 0;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
 }
